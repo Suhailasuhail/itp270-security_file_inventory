252b0d87ee37bf564ede3a87e13a7c64ae03af90
Suhaila <ss15267@email.vccs.edu>
2026-09-17 11:38:56 -0400

    add security file inventory script

--- security_file_inventory.py
+++ security_file_inventory.py
@@ -2,8 +2,14 @@ from pathlib import Path
 
 folder = Path("investigation_files")
 
-if folder.exists:
+if folder.exists() and folder.is_dir():
     print("this folder does exist!")
+
+    for item in folder.iterdir():
+        if item.is_file():
+            print("Name:", item.name)
+            print("Extension:", item.suffix)
+            print("Size:", item.stat().st_size, "bytes")
 
 else: 
     print("this folder does not exist!")
