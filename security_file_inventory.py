6fb30b5a6d118df89521bc7a4720493e760bc2fb
Suhaila <ss15267@email.vccs.edu>
2026-09-17 11:24:02 -0400

    added the hlib path and logic to test if poth exists

--- security_file_inventory.py
+++ security_file_inventory.py
@@ -1,1 +1,9 @@
-print("Testing")
+from pathlib import Path
+
+folder = Path("investigation_files")
+
+if folder.exists:
+    print("this folder does exist!")
+
+else: 
+    print("this folder does not exist!")
